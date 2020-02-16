e1781fbd23df97b1989cb15092bd1fc17ec93cd4
mazubar <61112655+mazubar@users.noreply.github.com>
2020-02-17 01:28:00 +0300

    Update notifications.py

--- features/steps/notifications.py
+++ features/steps/notifications.py
@@ -7,3 +7,17 @@ Scenario: Уведомление о начале регистрации
   Then: Приходит уведомление о начале регистрации и нужных стойках регистрации
 """
 
+from model import Account, Flight
+from datetime import datetime, timedelta
+
+@given('В систему занесена информация о рейсе и пользователь включил дефолтные уведомления)
+def step_impl(context):
+    assert Account.notifications = True
+
+@when('Начинается регистрация на рейс')
+def step_impl(context):
+    assert datetime.strptime(Flight.departure, '%Y%m%d%H%M') > (date - timedelta(seconds=5))
+
+@then('Приходит уведомление о начале регистрации и нужных стойках регистрации')
+def step_impl(context, reg_desk):
+        print("Регистрация началась! Стойки регистрации:" + reg_desk)
